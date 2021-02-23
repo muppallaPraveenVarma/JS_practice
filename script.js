b722f0c5d820ca4094a5e25c694234f377a8774a
Muppalla Praveen Varma <praveenvarma864@gmail.com>
2021-02-23 22:46:48 +0530

    added new script

---
+++ script.js
@@ -0,0 +1,7 @@
+    function add() {
+        var a = document.getElementById('first');
+        var b = document.getElementById('second');
+        var sum = a+b;
+        document.getElementById('sum').value = sum;
+    }
+
