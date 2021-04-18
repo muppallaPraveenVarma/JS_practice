eaae0eef3e13e326fb5b0999a49a0ab602265ffc
Muppalla Praveen Varma <praveenvarma864@gmail.com>
2021-04-18 20:46:37 +0530

    JS Hero Undefined

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 console.log('hi');
-function median(a) {
-    let m;
-    let f,g;
-    let n = a.length;
-    if (n % 2 == 0) {
-        f = a[(n/2)-1];
-        g = a[(n/2)];
-        m = (f+g)/2;
-        console.log(m);
-    }
-    else {
-        m = a[Math.floor(n / 2)];
-        console.log(m)
-    }
+let n;
+function hello(n) {
+ if (n===undefined){
+ console.log('Hello world!');
+ } 
+ else{
+ console.log( 'Hello ' + n + '!');
+ }
 }
 
-median([1, 2, 10, 100]);
+hello('nj');
 
 
